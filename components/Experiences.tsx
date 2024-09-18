10b1578b93c31dd5af75c763dcede361d7b237fb
MarvsTech <ramos.marvin@seedtech.ph>
2024-09-18 10:28:53 -0800

    Update Experiences.tsx

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -1,6 +1,7 @@
 import { workExperience } from '@/data'
 import React from 'react'
 import { MovingBorders } from './ui/MovingBorders'
+import Image from 'next/image'
 
 const Experiences = () => {
   return (
@@ -20,7 +21,13 @@ const Experiences = () => {
             duration={Math.floor(Math.random() * 10000) + 10000}
           >
             <div className='flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
-              <img src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16'/>
+              <Image 
+                src={card.thumbnail} 
+                alt={card.thumbnail} 
+                width={16}
+                height={16}
+                className='lg:w-32 md:w-20'
+              />
 
               <div className='lg:ms-5'>
                 <h1 className='text-start text-xl md:text-2xl font-bold'>{card.title}</h1>
